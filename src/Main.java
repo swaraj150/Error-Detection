9db702163e92a4cb670bb179e1579403a0bfb7a2
Swaraj Andhale <111890715+swaraj150@users.noreply.github.com>
2023-08-21 19:09:47 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
-
             Scanner sc = new Scanner(System.in);
-
             int choice;
-
             while(true) {
                 System.out.print("1.Parity Check\n2.Checksum\n3.CRC\nEnter choice: ");
                 choice = sc.nextInt();
                 if (choice >=0 || choice <= 3) break;
                 else System.out.println();
             }
-
-
             String type = switch (choice) {
                 case 1 -> "ParityCheck";
                 case 2 -> "CheckSum";
@@ -25,12 +18,8 @@ public class Main {
                 default -> "";
             };
             Sender sx = new Sender(type);
-
-
             Receiver rx = Network.transmit(sx, type);
-
             boolean isError = rx.detectError(type);
-
             if (isError) System.out.println("Data transmitted to the receiver had an error!");
             else System.out.println("Data was successfully transmitted to receiver!");
     }
